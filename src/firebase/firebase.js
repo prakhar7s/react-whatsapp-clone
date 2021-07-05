ebffedaabf913ae59439fb1905d4742545e0e524
prakhar7s <prakharshrivastava971@gmail.com>
2021-07-05 21:55:06 +0530

    firebase app credentials saved in env file

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyCVN8VPHmsmUsFA4fsuxQ5DO2PSLiJjFGg",
-  authDomain: "simple-todo-project-22a5f.firebaseapp.com",
-  projectId: "simple-todo-project-22a5f",
-  storageBucket: "simple-todo-project-22a5f.appspot.com",
-  messagingSenderId: "789242465295",
-  appId: "1:789242465295:web:575aa5be162a953a19277a",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
